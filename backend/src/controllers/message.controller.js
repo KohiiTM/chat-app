b01375674db9d5119b190ddb926798c47e4c5add
KohiiTM <jamesandallen.dev@gmail.com>
2025-02-20 00:24:03 -0800

    complete sidebar users endpoint

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,1 +1,15 @@
-export const getUsersForSidebar = async (req, res) => {};
+import User from "../models/user.model.js";
+
+export const getUsersForSidebar = async (req, res) => {
+  try {
+    const loggedInUserId = req.user._id;
+    const filtedUsers = await Userfind({ _id: { $ne: loggedInUserId } }).select(
+      "-password"
+    );
+
+    res.status(200).json(filteredUsers);
+  } catch (error) {
+    console.log("Error in getUsersForSidebar controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};

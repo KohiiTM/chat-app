8c40a6a5ace12ed1d0a2b5ef0149f108d9e9a9b1
KohiiTM <jamesandallen.dev@gmail.com>
2025-02-20 00:35:32 -0800

    get messages between two different users

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
 import { getUsersForSidebar } from "../controllers/user.controller.js";
+import { getMessages } from "../controllers/message.controller.js";
 
 const router = express.Router();
 
 router.get("/user", protectRoute, getUsersForSidebar);
+router.get("/:id", protectRoute, getMessages);
 
 export default router;
